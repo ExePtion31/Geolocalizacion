5b2047120b14b60b9b55c89ae0d74b3584198cc1
Giovanni Baquero <giovancito113@gmail.com>
2022-05-10 23:37:00 -0500

    :sparkles: Adding admin and students paths

---
+++ Backend/database/src/main/java/com/uninpahu/database/database/service/CommonServiceImpl.java
@@ -0,0 +1,30 @@
+package com.uninpahu.database.database.service;
+
+import com.uninpahu.database.database.entity.Student;
+import com.uninpahu.database.database.repository.IRepositoryStudents;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+import java.util.*;
+
+@Service
+@Transactional
+public class CommonServiceImpl implements ICommonService {
+    @Autowired
+    IRepositoryStudents repository;
+
+    @Override
+    public boolean existByCorreo(String correo) {
+        return repository.existsByCorreo(correo);
+    }
+
+    @Override
+    public Optional<Student> getByCorreo(String correo) {
+        return repository.getByCorreo(correo);
+    }
+
+    @Override
+    public Student save(Student student) {
+        return repository.save(student);
+    }
+}
